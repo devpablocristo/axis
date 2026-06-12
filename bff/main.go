afd1f61392e7c88156194d0f98b0611cad6202f3
Pablo <devpablocristo@gmail.com>
2026-06-12 19:05:02 -0300

    Use explicit STG BFF auth mode

--- bff/main.go
+++ bff/main.go
@@ -162,7 +162,7 @@ func (s *server) withAuth(next http.Handler) http.Handler {
 }
 
 func (s *server) authenticate(r *http.Request) (authn.Principal, error) {
-	if s.cfg.AuthMode == "dev" || s.cfg.AuthMode == "preview" {
+	if s.cfg.AuthMode == "dev" || s.cfg.AuthMode == "preview" || s.cfg.AuthMode == "stg" {
 		return authn.Principal{
 			OrgID:      firstNonEmpty(r.Header.Get("X-Dev-Org-ID"), s.cfg.DevOrgID),
 			Actor:      firstNonEmpty(r.Header.Get("X-Dev-User-ID"), s.cfg.DevUserID),
